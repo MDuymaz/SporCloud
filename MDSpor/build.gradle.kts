2aacd9960db1d825f7ed8e81f2bcfe35cb9458e9
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-16 23:11:51 +0300

    Create build.gradle.kts

---
+++ MDSpor/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 22
+
+cloudstream {
+    authors     = listOf("SporCloud")
+    language    = "tr"
+    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://e7.pngegg.com/pngimages/955/749/png-clipart-ape-monkey-cartoon-sad-monkey-face-mammal-vertebrate.png"
+}
