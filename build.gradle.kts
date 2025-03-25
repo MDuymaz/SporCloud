e9aecab82d53bf99e71ad4cdb418a5388bfa9814
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-25 09:02:13 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,8 +42,7 @@ subprojects {
 
     android {
         namespace = "com.SporCloud"
-    defaultConfig {
-        buildConfigField "String", "TMDB_API", "\"${TMDB_API_KEY}\""
+   
     }
         buildFeatures {
             buildConfig = true
