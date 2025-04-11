ddc0ae3813fb08ce312f9fa3da612d71de4cfb5e
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-04-12 02:47:21 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
+        classpath("com.android.tools.build:gradle:8.0.0") // Android Gradle Plugin sürümünü güncelledik
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
@@ -35,9 +33,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/MDuymaz/SporCloud")
-
         authors = listOf("SporCloud")
     }
 
@@ -48,10 +44,7 @@ subprojects {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
-        }
-
-        buildFeatures {
-            buildConfig = true  // Burada buildConfig desteğini etkinleştiriyoruz
+            buildConfig = true // buildConfig özelliği buraya eklendi
         }
 
         compileOptions {
@@ -77,12 +70,7 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
-        // Stubs for all Cloudstream classes
         cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
 
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
