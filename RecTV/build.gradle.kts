26ca2214c1ec6067d560a8c8082e43a8cc05b3d7
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-04-13 02:44:27 +0530

    Update build.gradle.kts

--- RecTV/build.gradle.kts
+++ RecTV/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 72
 
 cloudstream {
-    authors     = listOf("keyiflerolsun", "yusiqo", "inatchii", "JustRelaxable")
+    authors     = listOf("SporCloud")
     language    = "tr"
     description = "RecTv APK, Türkiye’deki en popüler Çevrimiçi Medya Akış platformlarından biridir. Filmlerin, Canlı Sporların, Web Dizilerinin ve çok daha fazlasının keyfini ücretsiz çıkarın."
 
