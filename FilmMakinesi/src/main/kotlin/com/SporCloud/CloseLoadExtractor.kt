8ca5be4cc6a5316ff406de0d20860e7b12dd3be1
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-04-13 03:18:12 +0530

    Update CloseLoadExtractor.kt

--- FilmMakinesi/src/main/kotlin/com/SporCloud/CloseLoadExtractor.kt
+++ FilmMakinesi/src/main/kotlin/com/SporCloud/CloseLoadExtractor.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.*
 import android.util.Base64
 
 private fun getm3uLink(data: String): String {
-    val first  = Base64.decode(data,Base64.DEFAULT).reversedArray()
+    val first  = Base64.decode(data, Base64.DEFAULT).reversedArray()
     val second = Base64.decode(first, Base64.DEFAULT)
     val result = second.toString(Charsets.UTF_8).split("|")[1]
 
@@ -20,7 +20,12 @@ open class CloseLoad : ExtractorApi() {
     override val mainUrl         = "https://closeload.filmmakinesi.de"
     override val requiresReferer = true
 
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
         val extRef = referer ?: ""
         Log.d("Kekik_${this.name}", "url » $url")
 
@@ -39,24 +44,25 @@ open class CloseLoad : ExtractorApi() {
         val rawScript        = getAndUnpack(obfuscatedScript)
         val regex = Regex("var player=this\\}\\);var(.*?);myPlayer\\.src")
         val matchResult = regex.find(rawScript)
+
         if (matchResult != null) {
             val extractedString = matchResult.groups[1]?.value?.trim()?.substringAfter("=\"")?.substringBefore("\"")
             val m3uLink = Base64.decode(extractedString, Base64.DEFAULT).toString(Charsets.UTF_8)
             Log.d("Kekik_${this.name}", "m3uLink » $m3uLink")
 
             callback.invoke(
-                ExtractorLink(
-                    source  = this.name,
-                    name    = this.name,
-                    url     = m3uLink,
-                    referer = mainUrl,
-                    quality = Qualities.Unknown.value,
-                    isM3u8  = true
-                )
+                newExtractorLink(
+                    source = this.name,
+                    name = this.name,
+                    url = m3uLink,
+                    type = ExtractorLinkType.M3U8
+                ) {
+                    headers = mapOf("Referer" to mainUrl)
+                    quality = Qualities.Unknown.value
+                }
             )
         } else {
             println("No match found")
         }
-
     }
 }
