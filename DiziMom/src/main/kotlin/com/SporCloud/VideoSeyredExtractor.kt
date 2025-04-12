666d8f9213d6c5081a78165f160c663c5166e488
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-04-13 03:20:39 +0530

    Update VideoSeyredExtractor.kt

--- DiziMom/src/main/kotlin/com/SporCloud/VideoSeyredExtractor.kt
+++ DiziMom/src/main/kotlin/com/SporCloud/VideoSeyredExtractor.kt
@@ -10,25 +10,32 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
 open class VideoSeyred : ExtractorApi() {
-    override val name            = "VideoSeyred"
-    override val mainUrl         = "https://videoseyred.in"
+    override val name = "VideoSeyred"
+    override val mainUrl = "https://videoseyred.in"
     override val requiresReferer = true
 
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        val videoId  = url.substringAfter("embed/").substringBefore("?")
-        val videoUrl = "${mainUrl}/playlist/${videoId}.json"
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val videoId = url.substringAfter("embed/").substringBefore("?")
+        val videoUrl = "$mainUrl/playlist/$videoId.json"
         Log.d("Kekik_${this.name}", "videoUrl » $videoUrl")
 
-        val responseRaw                          = app.get(videoUrl)
-        val responseList:List<VideoSeyredSource> = jacksonObjectMapper().readValue(responseRaw.text) ?: throw ErrorLoadingException("VideoSeyred")
-        val response                              = responseList[0]
+        val responseRaw = app.get(videoUrl)
+        val responseList: List<VideoSeyredSource> =
+            jacksonObjectMapper().readValue(responseRaw.text)
+                ?: throw ErrorLoadingException("VideoSeyred")
+        val response = responseList[0]
 
         for (track in response.tracks) {
             if (track.label != null && track.kind == "captions") {
                 subtitleCallback.invoke(
                     SubtitleFile(
                         lang = track.label,
-                        url  = fixUrl(track.file)
+                        url = fixUrl(track.file)
                     )
                 )
             }
@@ -36,36 +43,37 @@ open class VideoSeyred : ExtractorApi() {
 
         for (source in response.sources) {
             callback.invoke(
-                ExtractorLink(
-                    source  = this.name,
-                    name    = this.name,
-                    url     = source.file,
-                    referer = "${mainUrl}/",
-                    quality = Qualities.Unknown.value,
-                    type    = INFER_TYPE
-                )
+                newExtractorLink(
+                    source = this.name,
+                    name = this.name,
+                    url = source.file,
+                    type = ExtractorLinkType.M3U8
+                ) {
+                    headers = mapOf("Referer" to "$mainUrl/")
+                    quality = Qualities.Unknown.value
+                }
             )
         }
     }
 
     data class VideoSeyredSource(
-        @JsonProperty("image")   val image: String,
-        @JsonProperty("title")   val title: String,
+        @JsonProperty("image") val image: String,
+        @JsonProperty("title") val title: String,
         @JsonProperty("sources") val sources: List<VSSource>,
-        @JsonProperty("tracks")  val tracks: List<VSTrack>
+        @JsonProperty("tracks") val tracks: List<VSTrack>
     )
 
     data class VSSource(
-        @JsonProperty("file")    val file: String,
-        @JsonProperty("type")    val type: String,
+        @JsonProperty("file") val file: String,
+        @JsonProperty("type") val type: String,
         @JsonProperty("default") val default: String
     )
 
     data class VSTrack(
-        @JsonProperty("file")     val file: String,
-        @JsonProperty("kind")     val kind: String,
+        @JsonProperty("file") val file: String,
+        @JsonProperty("kind") val kind: String,
         @JsonProperty("language") val language: String? = null,
-        @JsonProperty("label")    val label: String?    = null,
-        @JsonProperty("default")  val default: String?  = null
+        @JsonProperty("label") val label: String? = null,
+        @JsonProperty("default") val default: String? = null
     )
 }
